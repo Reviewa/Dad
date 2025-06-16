209cc3ce812c0dc9356e03b5678bc300d8fec81c
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-06-16 19:58:59 +0800

    Update foodie.js

--- foodie.js
+++ foodie.js
@@ -1,6 +1,6 @@
 /*
 
-项目名称：Foodie美食相机
+项目名称：Foodie美食相机 
 下载地址：https://apps.apple.com/cn/app/foodie-%E7%BE%8E%E9%A3%9F%E7%9B%B8%E6%9C%BA/id1336411132
 [rewrite_local]
 ^https?:\/\/content-static\.snowcam\.cn\/.*\/assets url script-response-body https://raw.githubusercontent.com/Reviewa/Dad/main/foodie.js
